20ff833c33de253d8a8d4af38575efe6e92f10dd
Lukasz Szczesny <luk@wybcz.pl>
2016-04-16 12:53:39 +0200

    Add ansible playbook for setting up new machine

---
+++ setup/bootstrap.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+type -p ansible-playbook > /dev/null
+if [ $? -ne 0 ]; then
+	echo "Please install ansible on your machine:"
+	echo "e.g. apt-get install ansible"
+	exit 1
+fi
+
+export ANSIBLE_ERROR_ON_UNDEFINED_VARS=True
+
+ansible-playbook -i "localhost," -v -b desktop.yml
