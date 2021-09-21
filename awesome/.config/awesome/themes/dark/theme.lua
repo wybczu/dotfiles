754d1dcad9f1963a444b8a88e9cab05a9989749d
Lukasz Szczesny <luk@wybcz.pl>
2021-09-21 17:10:33 +0200

    [awesome] Update font in default theme

--- awesome/.config/awesome/themes/dark/theme.lua
+++ awesome/.config/awesome/themes/dark/theme.lua
@@ -30,7 +30,7 @@ theme.colors.green   = "#859900ff"
 -- }}}
 
 -- {{{ Styles
-theme.font      = "Terminus 8"
+theme.font      = "Monospace Regular 8"
 
 -- {{{ Colors
 theme.fg_normal  = theme.colors.base02
