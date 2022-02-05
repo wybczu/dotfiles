b1c43505b6d69b1df077f5ea61507d961a27b33d
Lukasz Szczesny <luk@wybcz.pl>
2022-02-05 17:19:45 +0100

    Fix offlineimap config and update IMAP server cert fingerprint

--- offlineimap/.offlineimap.py
+++ offlineimap/.offlineimap.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python
 # -*- coding: utf8 -*-
 
-import keyring
+import subprocess
+
+def get_password(service, user):
+    args = ["keyring", "get", service, user]
+    try:
+        return subprocess.check_output(args).strip()
+    except subprocess.CalledProcessError:
+        return ""
